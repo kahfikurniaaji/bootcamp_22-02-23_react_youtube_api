43fe6e90a75edc8e74e3813151adcae212e6f1ed
Kahfi Kurnia Aji <58323098+kahfikurniaaji@users.noreply.github.com>
2023-02-16 13:05:57 +0700

    React Change Value Text

--- src/index.js
+++ src/index.js
@@ -1,9 +1,61 @@
 // Import module yang dibuthkan
-// import React from "react";
-import {title, navbar} from "./component/navbar"
-import element from "./component/content/element"
-import renderById from "./util/react-render";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-// Merender html yang diperlukan
-renderById([title, navbar], "nav")
-renderById(element, "root")
+// Membuat root element
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+// Membuat function App
+const App = () => {
+  //   Inisialisasti state
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+
+  const [updatedName, setUpdateName] = useState(name);
+  const [updatedJob, setUpdateJob] = useState(job);
+
+  //   Membuat function handler
+  const handleName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleJob = (event) => {
+    setJob(event.target.value);
+  };
+
+  const handleClick = () => {
+    setUpdateName(name);
+    setUpdateJob(job);
+  };
+
+  //   Mereturn html yang diperlukan
+  return (
+    <div>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        onChange={handleName}
+        value={name}
+        className="form-control"
+      />
+      <label htmlFor="job">Job</label>
+      <input
+        type="text"
+        id="job"
+        name="job"
+        onChange={handleJob}
+        value={job}
+        className="form-control"
+      />
+      <h1>Nama : {updatedName}</h1>
+      <h1>Pekerjaan : {updatedJob}</h1>
+      <button onClick={handleClick}>Submit</button>
+    </div>
+  );
+};
+
+// Render App
+root.render(<App />);
